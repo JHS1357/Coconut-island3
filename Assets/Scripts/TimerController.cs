58aec94938fbdcee67fc4056df1c6eaaa1c34394
JHS1357 <86407259+JHS1357@users.noreply.github.com>
2021-08-18 17:43:51 +0900

    버튼으로 무기 변경(사운드 추가)

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class TimerController : MonoBehaviour
+{
+    public float runningTime = 0.0f;
+    Coroutine timer= null;
+
+    [NonSerialized] public static TimerController instance = null;
+    //Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            //DontDestroyOnLoad(this.gameObject);
+        }
+        //else Destroy(this.gameObject);
+        //TimerSet(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetTimer()
+    {
+        runningTime = 0.0f;
+    }
+    public void TimerSet()
+    {
+        runningTime = 0.0f;
+        //if (timer != null) StopCoroutine(timer);
+        timer = StartCoroutine(SetTimer());
+
+    }
+
+    IEnumerator SetTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            runningTime += 0.1f;
+            string minute = ((int)runningTime / 60).ToString("00");
+            string second = (runningTime % 60).ToString("00.0");
+            this.transform.GetComponentInChildren<Text>().text = (minute + ":" + second);
+        }
+    }
+
+
+}
